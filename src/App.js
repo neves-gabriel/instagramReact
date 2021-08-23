34fb2aa0ed60301a716f6d58437833191e1b52d0
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-08-23 14:19:29 -0300

    add: react components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from './Navbar';
+import Posts from './Posts';
+import Sidebar from './Sidebar';
+import Stories from './Stories';
+import Mobile_Background from './Mobile_Background';
+
+export default function App() {
+    return (
+        <div>
+			<Navbar />
+
+            <div class="corpo">
+                <div class="esquerda">
+                    <Stories />
+                    <Posts />
+                </div>
+                <Sidebar />
+            </div>
+			<Mobile_Background />
+		</div>
+    );
+}
